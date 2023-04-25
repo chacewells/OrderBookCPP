cd49e28329e15d0d35411c6cebcc2a8d09bd4f57
chace <chacewells@gmail.com>
2023-04-24 20:21:53 -0700

    Simple DDL script

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <sqlite3.h>
 
+#define ORDER_BOOK_DB_FILE "orderbook.sqlite3"
+#define SCHEMA_SQL "CREATE TABLE orderbook (" \
+"id INTEGER PRIMARY KEY, " \
+"ticker_symbol TEXT, " \
+"side TEXT, " \
+"amount INTEGER, " \
+"price NUMERIC, " \
+"total NUMERIC);"
+
+
 int main() {
-    std::cout << sqlite3_libversion() << std::endl;
+    // get sqlite3 handle
+    sqlite3 *db;
+    sqlite3_open(ORDER_BOOK_DB_FILE, &db);
+
+    // create schema
+    char *errmsg;
+    sqlite3_exec(db, SCHEMA_SQL, nullptr, nullptr, &errmsg);
+    if (errmsg) {
+        std::cerr << errmsg << std::endl;
+        sqlite3_close(db);
+        return 1;
+    }
+
+    // release sqlite3 resource
+    sqlite3_close(db);
+
     return 0;
 }
